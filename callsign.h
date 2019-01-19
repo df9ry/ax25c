50185cb6e39800b2a6a4b096c1954027709c5677
Tania Hagn <info@taniahagn.de>
2019-01-19 18:12:15 +0100

    Implemented callsign routines

---
+++ callsign.h
@@ -0,0 +1,187 @@
+/*
+ *  Project: ax25c - File: callsign.h
+ *  Copyright (C) 2019 - Tania Hagn - tania@df9ry.de
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file callsign.h
+ * @brief Support for managing callsigns.
+ */
+#ifndef CALLSIGN_H_
+#define CALLSIGN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+struct exception;
+
+/**
+ * @brief Internal representation of callsigns.
+ */
+typedef uint64_t callsign;
+
+struct addressField {
+	callsign destination;
+	callsign source;
+	callsign repeaters[2];
+};
+
+/**
+ * @brief Encode a callsign from string notation.
+ * @param str String notation to encode into a callsign.
+ * @param ex Exception struct, optional.
+ * @return Encoded callsign or 0, in the case of an error. Param ex will
+ *         contain detailed information about the problem otherwise.
+ */
+extern callsign callsignFromString(const char *str, struct exception *ex);
+
+/**
+ * @brief Decode a callsign to a string buffer.
+ * @param call Internal callsign representation to decode.
+ * @param pb Pointer to target char buffer.
+ * @param cb Size of the target char buffer.
+ * @param ex Exception struct, optional.
+ * @return Number of bytes used from buffer. When -1, an error happened.
+ *         Param ex will contain detailed Information about the problem.
+ */
+extern int callsignToString(callsign call, char *pb, size_t cb,
+								struct exception *ex);
+
+/**
+ * @brief Encode an addressField from string notation.
+ * @param source Source callsign.
+ * @param dest String notation to encode destination into an addressField.
+ * @param af Pointer to addressField to encode to.
+ * @param ex Exception struct, optional.
+ * @return True when encoding was successful. Param ex will
+ *         contain detailed information about the problem otherwise.
+ */
+extern bool addressFieldFromString(callsign source, const char *dest,
+		struct addressField *af, struct exception *ex);
+
+/**
+ * @brief Decode an adressField into string notation.
+ * @param af AddressField to decode.
+ * @param pb Pointer to string buffer.
+ * @param cb Size of string buffer.
+ * @param ex Exception struct, optional.
+ * @return True when decoding was successful. Param ex will
+ *         contain detailed information about the problem otherwise.
+ */
+extern bool addressFieldToString(const struct addressField *af,
+									char *pb, size_t cb,
+									struct exception *ex);
+
+/**
+ * @brief Get the H-Bit of a callsign.
+ * @param call The callsign to investigate.
+ * @return H-Bit.
+ */
+static inline bool getHBit(callsign call)
+{
+	return (((uint8_t*)(&call))[6] & 0x80);
+}
+
+/**
+ * @brief Set the H-Bit of a callsign.
+ * @param call The callsign to modify.
+ * @param h Value of H-Bit to set.
+ * @return Modified callsign.
+ */
+static inline callsign setHBit(callsign call, bool h)
+{
+	if (h)
+		((uint8_t*)(&call))[6] |= 0x80;
+	else
+		((uint8_t*)(&call))[6] &= ~0x80;
+	return call;
+}
+
+/**
+ * @brief Get the X-Bit of a callsign.
+ * @param call The callsign to investigate.
+ * @return X-Bit.
+ */
+static inline bool getXBit(callsign call)
+{
+	return (((uint8_t*)(&call))[6] & 0x01);
+}
+
+/**
+ * @brief Set the X-Bit of a callsign.
+ * @param call The callsign to modify.
+ * @param x Value of X-Bit to set.
+ * @return Modified callsign.
+ */
+static inline callsign setXBit(callsign call, bool x)
+{
+	if (x)
+		((uint8_t*)(&call))[6] |= 0x10;
+	else
+		((uint8_t*)(&call))[6] &= ~0x10;
+	return call;
+}
+
+/**
+ * @brief Get the C-Bit of a callsign.
+ * @param call The callsign to investigate.
+ * @return C-Bit.
+ */
+static inline bool getCBit(callsign call)
+{
+	return (((uint8_t*)(&call))[6] & 0x80);
+}
+
+/**
+ * @brief Set the C-Bit of a callsign.
+ * @param call The callsign to modify.
+ * @param c Value of C-Bit to set.
+ * @return Modified callsign.
+ */
+static inline callsign setCBit(callsign call, bool c)
+{
+	if (c)
+		((uint8_t*)(&call))[6] |= 0x80;
+	else
+		((uint8_t*)(&call))[6] &= ~0x80;
+	return call;
+}
+
+/**
+ * @brief Get the number of digipeaters of an addressField.
+ * @param af AddressField to investigate.
+ * @return Number of digipeaters in addressField.
+ */
+static inline int getNRepeaters(struct addressField *af)
+{
+	if (getXBit(af->source))
+		return 0;
+	else if (getXBit(af->repeaters[0]))
+		return 1;
+	else
+		return 2;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CALLSIGN_H_ */
---
+++ runtime/callsign.c
@@ -0,0 +1,295 @@
+/*
+ *  Project: ax25c - File: callsign.c
+ *  Copyright (C) 2019 - Tania Hagn - tania@df9ry.de
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../callsign.h"
+#include "../exception.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <assert.h>
+
+#define MODULE_NAME "callsign"
+
+union _callsign {
+	callsign encoded;
+	uint8_t octets[7];
+};
+
+static void fillEx(struct exception *ex, const char *fn, const char *msg,
+					const char *par, int erc)
+{
+	assert(fn);
+	assert(msg);
+	if (ex) {
+		ex->erc = erc;
+		ex->module = MODULE_NAME;
+		ex->function = fn;
+		ex->message = msg;
+		ex->param = par ? par : "";
+	}
+}
+
+static uint8_t getOctetOfChar(char ch)
+{
+
+	if ((ch >= 'a') && (ch <= 'z'))
+		ch -= 0x20;
+	if ( ((ch >= '0') && (ch <= '9')) || ((ch >= 'A') && (ch <= 'Z')) )
+		return ch << 1;
+	return 0;
+}
+
+callsign callsignFromString(const char *str, struct exception *ex)
+{
+	size_t i = 0;
+	uint8_t octet;
+	long int _ssid;
+	char *_call;
+	union _callsign c;
+
+	assert(str);
+	c.encoded = 0;
+	while (*str != '\0') {
+		if (i == 6) {
+			if (*str == '-') {
+				++str;
+				++i;
+				break;
+			}
+			fillEx(ex, "callsignFromString",
+					"Callsign too long (max. 6 characters)", str,
+					EXIT_FAILURE);
+			return 0;
+		}
+		octet = getOctetOfChar(*str);
+		if (octet == 0) {
+			fillEx(ex, "callsignFromString",
+					"Invalid callsign character", str,
+					EXIT_FAILURE);
+			return 0;
+		}
+		c.octets[i] = octet;
+		++str;
+		++i;
+	} /* end while */
+	if (i == 0) {
+		fillEx(ex, "callsignFromString",
+				"Callsign too short (min. 1 character)", str,
+				EXIT_FAILURE);
+		return 0;
+	}
+	while (i++ < 6)
+		c.octets[i] = 0x40;
+	if (*str != '\0') {
+		_ssid = strtol(str, &_call, 0);
+		if ((_ssid < 0) || (_ssid >= 16)) {
+			fillEx(ex, "callsignFromString",
+					"SSID is out of range (0 .. 15)", str,
+					EXIT_FAILURE);
+			return 0;
+		}
+	}
+	if (*_call != '\0') {
+		fillEx(ex, "callsignFromString",
+				"Extra characters after callsign", str,
+				EXIT_FAILURE);
+		return 0;
+	}
+	c.octets[6] = 0x60 | (_ssid << 1);
+	return c.encoded;
+}
+
+static int __tooShort(const char *func, struct exception *ex)
+{
+	assert(func);
+	fillEx(ex, func, "Buffer too short", NULL, EXIT_FAILURE);
+	return -1;
+}
+
+int callsignToString(callsign call, char *pb, size_t cb,
+							struct exception *ex)
+{
+	union _callsign c;
+	char ch;
+	size_t i, cb1;
+	int ssid;
+
+	assert(call);
+	assert(pb);
+	assert(cb);
+	cb1 = cb;
+	c.encoded = call;
+	for (i = 0; i < 6; ++i) {
+		ch = (c.octets[i] & 0xfe) >> 1;
+		if (ch != ' ') {
+			if (cb-- == 0)
+				return __tooShort("callsignToString", ex);
+			*pb++ = ch;
+		}
+	} /* end for */
+	if (cb-- == 0)
+		return __tooShort("callsignToString", ex);
+	*pb++ = '-';
+	ssid = (c.octets[6] & 0x1e) >> 1;
+	i = snprintf(pb, cb, "%i", ssid);
+	if (i > cb)
+		return __tooShort("callsignToString", ex);
+	i -= 1;
+	cb -= i;
+	pb += i;
+	if (getHBit(call)) {
+		if (cb-- == 0)
+			return __tooShort("callsignToString", ex);
+		*pb++ = '*';
+	}
+	if (cb-- == 0)
+		return __tooShort("callsignToString", ex);
+	*pb++ = '\0';
+	return cb1 - cb;
+}
+
+static const char *__skipWhitespace(const char *str)
+{
+	while ((*str) && isspace(*str))
+		++str;
+	return str;
+}
+
+static const char *__skipNoWhitespace(const char *str)
+{
+	while ((*str) && (!isspace(*str)))
+		++str;
+	return str;
+}
+
+bool addressFieldFromString(callsign source, const char *dest,
+		struct addressField *af, struct exception *ex)
+{
+	assert(source);
+	assert(dest);
+	assert(af);
+	memset(af, 0x00, sizeof(struct addressField));
+	af->source = source;
+	dest = __skipWhitespace(dest);
+
+	/* Read destination */
+	af->destination = callsignFromString(dest, ex);
+	if (!af->destination)
+		return false;
+	dest = __skipWhitespace(__skipNoWhitespace(dest));
+	if (!(*dest)) {
+		af->source = setXBit(af->source, true);
+		return true;
+	}
+
+	/* Read repeater 1 */
+	af->repeaters[0] = callsignFromString(dest, ex);
+	if (!af->repeaters[0])
+		return false;
+	dest = __skipWhitespace(__skipNoWhitespace(dest));
+	if (!(*dest)) {
+		af->repeaters[0] = setXBit(af->repeaters[0], true);
+		return true;
+	}
+
+	/* Read repeater 2 */
+	af->repeaters[1] = callsignFromString(dest, ex);
+	if (!af->repeaters[1])
+		return false;
+	dest = __skipWhitespace(__skipNoWhitespace(dest));
+	if (!(*dest)) {
+		af->repeaters[1] = setXBit(af->repeaters[1], true);
+		return true;
+	}
+
+	/* Error */
+	if (ex) {
+		ex->erc = EXIT_FAILURE;
+		ex->module = MODULE_NAME;
+		ex->function = "addressFieldFromString";
+		ex->message = "Too many repeaters (max. 2)";
+		ex->param = dest;
+		return false;
+	}
+
+	return true;
+}
+
+bool addressFieldToString(const struct addressField *af,
+									char *pb, size_t cb,
+									struct exception *ex)
+{
+	int cb1;
+
+	assert(af);
+	assert(pb);
+	assert(cb);
+
+	cb1 = callsignToString(af->destination, pb, cb, ex);
+	if (cb1 < 0)
+		return false;
+	cb -= cb1;
+	pb += cb1;
+	if (cb++ == 0)
+		return (__tooShort("addressFieldToString", ex) == 0);
+	*pb++ = ' ';
+
+	cb1 = callsignToString(af->source, pb, cb, ex);
+	if (cb1 < 0)
+		return false;
+	cb -= cb1;
+	pb += cb1;
+	if (cb++ == 0)
+		return (__tooShort("addressFieldToString", ex) == 0);
+	if (getXBit(af->source)) {
+		*pb++ = '\0';
+		return true;
+	}
+	*pb++ = ' ';
+
+	cb1 = callsignToString(af->repeaters[0], pb, cb, ex);
+	if (cb1 < 0)
+		return false;
+	cb -= cb1;
+	pb += cb1;
+	if (cb++ == 0)
+		return (__tooShort("addressFieldToString", ex) == 0);
+	if (getXBit(af->repeaters[0])) {
+		*pb++ = '\0';
+		return true;
+	}
+	*pb++ = ' ';
+
+	cb1 = callsignToString(af->repeaters[1], pb, cb, ex);
+	if (cb1 < 0)
+		return false;
+	cb -= cb1;
+	pb += cb1;
+	if (cb++ == 0)
+		return (__tooShort("addressFieldToString", ex) == 0);
+	if (getXBit(af->repeaters[1])) {
+		*pb++ = '\0';
+		return true;
+	}
+	if (cb < 5)
+		return (__tooShort("addressFieldToString", ex) == 0);
+
+	strncpy(pb, " ...", cb);
+	return true;
+}
